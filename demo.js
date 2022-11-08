841c4030afd9ec647be6f9e08400af9425c4941e
Aditya_khare <adikhare24@gmail.com>
2022-11-08 20:55:58 +0530

    changed demo.js

--- demo.js
+++ demo.js
@@ -1,1 +1,8 @@
 console.log("I love git")
+
+const headerTitle = document.getElementById('header-title')
+const title1 = document.getElementsByClassName('title')
+
+headerTitle.style.borderBottom = 'solid 3px black'
+title1[0].style.fontWeight = 'bold'
+title1[0].style.color = 'green'
