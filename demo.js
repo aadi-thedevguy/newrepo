696e9b0e9ae13427e4f328d9d2369a2bd67306c3
Aditya_khare <adikhare24@gmail.com>
2022-11-08 21:26:50 +0530

    added for loop

--- demo.js
+++ demo.js
@@ -2,7 +2,14 @@ console.log("I love git")
 
 const headerTitle = document.getElementById('header-title')
 const title1 = document.getElementsByClassName('title')
+const items = document.getElementsByClassName('list-group-item')
 
 headerTitle.style.borderBottom = 'solid 3px black'
 title1[0].style.fontWeight = 'bold'
 title1[0].style.color = 'green'
+
+items[2].style.backgroundColor = 'green';
+for (let i = 0; i < items.length; i++) {
+   items[i].style.fontWeight = 'bold'
+    
+}
