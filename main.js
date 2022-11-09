d78bd7674e8d9911fa69f7049d6138e8312b2a98
Aditya_khare <adikhare24@gmail.com>
2022-11-09 20:03:25 +0530

    edit/delete

--- main.js
+++ main.js
@@ -7,10 +7,11 @@ const userList = document.querySelector('#users');
 // Listen for form submit
 myForm.addEventListener('submit', onSubmit);
 
+window.onload = getData()
 function onSubmit(e) {
   e.preventDefault();
-  
-  if(nameInput.value === '' || emailInput.value === '') {
+
+  if (nameInput.value === '' || emailInput.value === '') {
     // alert('Please enter all fields');
     msg.classList.add('error');
     msg.innerHTML = 'Please enter all fields';
@@ -20,25 +21,43 @@ function onSubmit(e) {
   } else {
     // Create new list item with user
     let user = {
-      name : nameInput.value,
-      email : emailInput.value
+      name: nameInput.value,
+      email: emailInput.value
     }
-    
+
     localStorage.setItem("userDetails" + user.email, JSON.stringify(user))
-    userList.innerHTML = userList.innerHTML + `<li>${user.name}: ${user.email}</li> `
-        // Clear fields
-        nameInput.value = '';
-        emailInput.value = '';
-      
+    userList.innerHTML = userList.innerHTML + `<li id=${user.email}>${user.name}: ${user.email} 
+    <button class="btn delete" onclick=deleteUser('${user.email}')>X</button>
+    <button class="btn edit" onclick=editUserDetails('${user.email}','${user.name}')>Edit</button></li> `
+    // Clear fields
+    nameInput.value = '';
+    emailInput.value = '';
+
   }
 }
-function getData () {
+
+function getData() {
   let keys = Object.keys(localStorage);
-  for(let key of keys) {
-    let {name,email} = JSON.parse(localStorage.getItem(key))
-    userList.innerHTML = userList.innerHTML + `<li>${name}: ${email}</li> `
+  for (let key of keys) {
+    let { name, email } = JSON.parse(localStorage.getItem(key))
+    userList.innerHTML = userList.innerHTML + `<li id=${email}>${name}: ${email} 
+    <button class="btn delete" onclick= deleteUser('${email}')>X</button>
+    <button class="btn edit" onclick=editUserDetails('${email}','${name}')>Edit</button></li>`
   }
 
 }
 
-getData()
+function deleteUser(emailId) {
+  localStorage.removeItem("userDetails" + emailId)
+  const toDelete = document.getElementById(emailId);
+  userList.removeChild(toDelete)
+
+}
+
+function editUserDetails(emailId, name) {
+
+  emailInput.value = emailId;
+  nameInput.value = name;
+
+  deleteUser(emailId)
+}
