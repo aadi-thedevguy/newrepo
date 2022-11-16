23a5a87ec0a4e815b89a29a576cd45a07eac31f2
Aditya_khare <adikhare24@gmail.com>
2022-11-16 11:47:41 +0530

    expense tracker

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 // Put DOM elements into variables
 const myForm = document.querySelector('#my-form');
-const nameInput = document.querySelector('#name');
-const emailInput = document.querySelector('#email');
+const descriptionInput = document.querySelector('#description');
+const amountInput = document.querySelector('#amount');
+const categoriesInput = document.querySelector('#category');
 const msg = document.querySelector('.msg');
-const userList = document.querySelector('#users');
+const list = document.querySelector('#list');
 // Listen for form submit
 myForm.addEventListener('submit', onSubmit);
 
-window.onload = getData()
 function onSubmit(e) {
   e.preventDefault();
 
-  if (nameInput.value === '' || emailInput.value === '') {
+  if (descriptionInput.value === '' || amountInput.value === '') {
     // alert('Please enter all fields');
     msg.classList.add('error');
     msg.innerHTML = 'Please enter all fields';
@@ -19,45 +19,38 @@ function onSubmit(e) {
     // Remove error after 3 seconds
     setTimeout(() => msg.remove(), 3000);
   } else {
-    // Create new list item with user
-    let user = {
-      name: nameInput.value,
-      email: emailInput.value
+
+    // Create new list item 
+    let li = document.createElement('li')
+    let details = {
+      amount : amountInput.value,
+      desc : descriptionInput.value,
+      category: categoriesInput.value
     }
-
-    localStorage.setItem("userDetails" + user.email, JSON.stringify(user))
-    userList.innerHTML = userList.innerHTML + `<li id=${user.email}>${user.name}: ${user.email} 
-    <button class="btn delete" onclick=deleteUser('${user.email}')>X</button>
-    <button class="btn edit" onclick=editUserDetails('${user.email}','${user.name}')>Edit</button></li> `
-    // Clear fields
-    nameInput.value = '';
-    emailInput.value = '';
+    li.innerHTML = `
+    <div id=${amountInput.value}>
+      ${details.amount} - ${details.desc} - ${details.category} <button class="btn delete" onclick=deleteItem('${amountInput.value}')>Delete</button>
+      <button class="btn edit" onclick=edit('${details.amount}','${details.desc}')>Edit</button>
+    </div>
+    `
+    list.appendChild(li)
+    
 
   }
 }
 
-function getData() {
-  let keys = Object.keys(localStorage);
-  for (let key of keys) {
-    let { name, email } = JSON.parse(localStorage.getItem(key))
-    userList.innerHTML = userList.innerHTML + `<li id=${email}>${name}: ${email} 
-    <button class="btn delete" onclick= deleteUser('${email}')>X</button>
-    <button class="btn edit" onclick=editUserDetails('${email}','${name}')>Edit</button></li>`
-  }
+
+function deleteItem(amount) {
+ 
+  const toDelete = document.getElementById(amount)
+  toDelete.style.display = 'none'
 
 }
 
-function deleteUser(emailId) {
-  localStorage.removeItem("userDetails" + emailId)
-  const toDelete = document.getElementById(emailId);
-  userList.removeChild(toDelete)
 
-}
+function edit(amount, description) {
+  amountInput.value = amount
+  descriptionInput.value = description
 
-function editUserDetails(emailId, name) {
-
-  emailInput.value = emailId;
-  nameInput.value = name;
-
-  deleteUser(emailId)
+  deleteItem(amount)
 }
